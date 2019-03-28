0b4142a41b504608cba12f40af94725a23b0430b
Existentialist-Robot <eredman@ualberta.ca>
2019-03-28 16:00:26 -0600

    add .gitignore + modify pi_amp latencies.m

--- Pi3_Amp_Latencies/Pi_Amp_Latencies.m
+++ Pi3_Amp_Latencies/Pi_Amp_Latencies.m
@@ -7,45 +7,53 @@ filename = ['testing_visor_pi_005.vhdr'];
 EEG = pop_loadbv(filepath, filename, [], []);
 [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname','timingtest','gui','off');
 
+trials = 500;
 start_trigger = ALLEEG(1).event(2).latency;
-EEG_latencies = zeros(1,(length(ALLEEG(1).event)-3));
+EEG_latencies = zeros(1,trials);
+EEG_latencies = [];
+EEG_latency = 0;
+county = 1;
+
+% for i_event = 3:(length(ALLEEG(1).event)-1)
+%     if EEG.event(i_event).type(4) == '1' || EEG.event(i_event).type(4) == '2'
+%         EEG_latencies(1,i_event-2) = (EEG(1).event(i_event).latency - start_trigger)/ALLEEG.srate;
+%         EEG_latencies(2,i_event-2) = EEG.event(i_event).type(4);
+%     else
+%         EEG_latencies(i_event-2) = 0;
+%     end
+% end
 
 for i_event = 3:(length(ALLEEG(1).event)-1)
-    
-    EEG_latencies(i_event-2) = (ALLEEG(1).event(i_event).latency - start_trigger)/ALLEEG.srate;
-    
+    if ALLEEG(1).event(i_event).type(4) == '1' || ALLEEG(1).event(i_event).type(4) == '2'
+        EEG_latency = (ALLEEG(1).event(i_event).latency - start_trigger)/ALLEEG.srate;
+        EEG_latencies(county) = EEG_latency;
+        county = county + 1;
+    end
 end
 
 %%%now let's load the times recorded by the pi%%%
 %%%need to subtract 5 from these since there is 5 seconds before
 %%%the red LEDs, indicating the start, are turned on%%%
-pi_trials = 500;
 % % % trig_type,trig_time, delay_length, trial_resp, jitter_length, first_light_difference, second_light_difference
-pi_recorded_times = csvread('C:\Users\User\Documents\GitHub\GoPro_Visor_Pi\Pi3_Amp_Latencies\Pi_Time_Data\data004_visual_p3_gopro_visor.csv',0,1,[0,1,6,pi_trials]); %,1,0,[1,0,1,249]
+pi_recorded_times = csvread('C:\Users\User\Documents\GitHub\GoPro_Visor_Pi\Pi3_Amp_Latencies\Pi_Time_Data\data005_visual_p3_gopro_visor.csv',0,1,[0,1,6,trials]); %,1,0,[1,0,1,249]
 %%% pi_recorded_times = pi_recorded_times - 5;
-pi_type = pi_recorded_times(1,:);
-pi_latency = pi_recorded_times(2,:);
-pi_delay = pi_recorded_times(3,:);
-pi_resp = pi_recorded_times(4,:);
-pi_jitter = pi_recorded_times(5,:);
-
-
-
-
+pi_type = pi_recorded_times(1,:); % trig type
+pi_latency = pi_recorded_times(2,:); % trig time latency
+pi_delay = pi_recorded_times(3,:); % delay length
+pi_resp = pi_recorded_times(4,:); % trial resp
+pi_jitter = pi_recorded_times(5,:); % jitter length
+pi_start_stop = pi_recorded_times(6,:); % resp_latency
 all_latencies(1,:) = EEG_latencies;
-all_latencies(2,:) = pi_recorded_times;
+all_latencies(2,:) = pi_latency;
 
 conditions = {'EEG','Pi Times'};
 
+%%%plot the latency of both pi and amp
 figure;hold on;
 colours = ['k','b'];
 
-for i_plot = 1:2
+plot(all_latencies(i_plot,:),[1:trials],'color',colours(i_plot));
     
-    plot(all_latencies(i_plot,:),[1:250],'color',colours(i_plot));
-    
-end
-
 xlabel('Time (Seconds)');ylabel('Trial');legend('EEG','Pi Times');
 hold off;
 
@@ -55,7 +63,11 @@ conditions = {'EEG - Pi Times'};
 figure;hold on;
 colours = ['k','b'];
 
-plot(all_latencies(1,:)-all_latencies(i_plot,:),[1:250],'color',colours(i_plot));
+plot(all_latencies(1,:)-all_latencies(2,:),[1:trials],'color',colours(i_plot));
+
+%%% GoPro linear regression 
+%%%mdl = LinearModel.fit(all_latencies(2,:),all_latencies(1,:),'linear'); %%% confirm what's a happen!
+mdl = fitlm(all_latencies(2,:),all_latencies(1,:),'linear'); %%% confirm what's a happen!
 
 xlabel('Time (Seconds)');ylabel('Trial');legend('EEG - Pi Times');
 hold off;
