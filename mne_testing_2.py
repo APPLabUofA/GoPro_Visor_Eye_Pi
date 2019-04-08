e6d72a6f6f2a0be68a9799fc17aacccc9e9216e7
Existentialist-Robot <eredman@ualberta.ca>
2019-04-08 16:52:41 -0600

    Update mne_testing_2.py

--- mne_testing_2.py
+++ mne_testing_2.py
@@ -3,6 +3,7 @@
 import mne
 import numpy as np
 import matplotlib.pyplot as plt
+from mne.time_frequency import tfr_morlet
 
 #define some constants#
 eeg_chan_num = 16
@@ -12,10 +13,10 @@ tmin, tmax = -0.2, 1.0
 baseline = (None, 0.0)
 reject1 = {'eeg': 1000, 'eog': 1000}
 reject2 = {'eeg': 500, 'eog': 500}
-plot_max_standards = 20
-plot_min_standards = -10
-plot_max_targets = 15
-plot_min_targets = -5
+plot_max_standards = 20 
+plot_min_standards = -20#-10 
+plot_max_targets = 20
+plot_min_targets = -20#-5
 electrodes = ['Pz']
 colours = ['black', 'red']
 conditions = ["Standards", "Targets"]
@@ -27,18 +28,18 @@ raw = mne.io.read_raw_brainvision(filename)
 
 #specify montage, EOGs, and reference#
 raw.set_montage('standard_1020')
-raw.set_channel_types(mapping={'HEOG': 'eog'})
+raw.set_channel_types(mapping={'HEOG': 'eog'}) # set EOGs as such
 raw.set_channel_types(mapping={'VEOG': 'eog'})
-mne.set_eeg_reference(raw.load_data(), ref_channels=['M2'])
+mne.set_eeg_reference(raw.load_data(), ref_channels=['M2']) # reference to mastoid
 
 #define our times, EEG and EOG channels#
 eeg_data = raw[:eeg_chan_num,:][0]
 eog_data = raw[eeg_chan_num:eeg_chan_num + 2,:][0]
-times = raw[0,:][1]
+times = raw[0,:][1] # all times in 1 millisecond increments
 
 #filter our raw data#
 fmin, fmax = 0, 30 #in Hz
-raw.filter(fmin, fmax)
+raw.filter(fmin, fmax) # high pass and low pass filters
 
 #get our epochs#
 picks = mne.pick_types(raw.info, eeg=True, eog=True)
@@ -57,7 +58,8 @@ standards.plot(spatial_colors=True, gfp=True,
                 ylim=dict(eeg=[plot_max_standards,plot_min_standards]), xlim='tight', titles='Standard ERPs')
 targets.plot(spatial_colors=True, gfp=True, 
                 ylim=dict(eeg=[plot_max_targets,plot_min_targets]), xlim='tight', titles='Target ERPs')
-
+#Topoplot
+targets.plot_topomap(times=[0.4], size=3., title='Topoplot (Pz) @ 300 ms Post-stim', time_unit='s') #  titles='Topoplot'
 #now let's compare our standard and target ERPs#
 evoked_dict = dict(standards = epochs['standards'].average(), targets = epochs['targets'].average())
 
@@ -74,24 +76,57 @@ ch_names = epochs['standards'].average().ch_names
 plt.plot(difference_waveform[[ch_names.index(i) for i in electrodes],:])
 
 
-
-
-
-
-
+target, standard = epochs["targets"].average, epochs["standards"].average
+joint_kwargs = dict(ts_args=dict(time_unit='s'),
+                    topomap_args=dict(time_unit='s'))
+mne.combine_evoked([targets, standards], weights='equal').plot_joint(**joint_kwargs)
 
 #n_fft = 1024 # FFT size, should be a power of 2
 #raw.notch_filter(np.arange(60, 241, 60), picks=picks, filter_length='auto',
 #                 phase='zero')
 
 #get our event information#
+
+
+
+########################################################### Start of PSD plots - still need to adapt
+#frequencies =  np.linspace(6, 30, 100, endpoint=True)
+#
+#wave_cycles = 6
+#
+# # Compute morlet wavelet
+#
+## Left Cue
+#tfr, itc = tfr_morlet(epochs['LeftCue'], freqs=frequencies, 
+#                      n_cycles=wave_cycles, return_itc=True)
+#tfr = tfr.apply_baseline([-1,-.5],mode='mean')
+#tfr.plot(picks=[0], mode='logratio', 
+#         title='TP9 - Ipsi');
+#tfr.plot(picks=[1], mode='logratio', 
+#         title='TP10 - Contra');
+#power_Ipsi_TP9 = tfr.data[0,:,:]
+#power_Contra_TP10 = tfr.data[1,:,:]
+#
+## Right Cue
+#tfr, itc = tfr_morlet(epochs['RightCue'], freqs=frequencies, 
+#                      n_cycles=wave_cycles, return_itc=True)
+#tfr = tfr.apply_baseline([-1,-.5],mode='mean')
+#tfr.plot(picks=[0], mode='logratio', 
+#         title='TP9 - Contra');
+#tfr.plot(picks=[1], mode='logratio', 
+#         title='TP10 - Ipsi');
+#power_Contra_TP9 = tfr.data[0,:,:]
+#power_Ipsi_TP10 = tfr.data[1,:,:]
+###############################################
+
 order = np.arange(raw.info['nchan'])
 events = mne.find_events(raw)
 
 mne.viz.plot_events(events, raw.info['sfreq'], raw.first_samp, color=colour,
                     event_id=event_id)
 raw.plot(events=events, n_channels=18, order=order)
-
+#raw.plot_psd();
+raw.plot_psd(fmin=1, fmax=30);
 #perform a regression-based eye blink correction#
 #right now this occurs on all the data#
 #might want to epoch data and perform one round of artifact rejection first#
@@ -106,3 +141,8 @@ raw.plot(n_channels=16, start=54, duration=60,
 
 corrected_raw.plot(n_channels=16, start=54, duration=60, 
               scalings=dict(eeg=50e-6))
+
+
+
+
+
