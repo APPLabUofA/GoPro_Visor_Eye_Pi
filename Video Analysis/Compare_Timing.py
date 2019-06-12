d967db31cf4a0d7d8a0c451544d2c6aaf2996f52
Existentialist-Robot <eredman@ualberta.ca>
2019-06-12 10:04:56 -0600

    dump

--- Video Analysis/Compare_Timing.py
+++ Video Analysis/Compare_Timing.py
@@ -7,13 +7,13 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
 # %% Define Variables
-par = 3
+par = 1
 diff_outlier = 0 # 0 = off, 1 == on - automatically take out outliers in jitter between camera and eeg 
 outlier_def = 10 # number seconds of required jitter in order to be automatically disregarded
 
 # %% Load in the Camera timing form the temporary workspace or cvs file for the participant you are working with
 # If you want to work with group data - refer to Group_Figures.py
-df1 = pd.read_csv((r'M:\Data\GoPro_Visor\Experiment_1\Video_Times\Dataframe_df1a_00' + str(par)) + '.csv', sep=',') # pilot
+df1 = pd.read_csv((r'M:\Data\GoPro_Visor\Experiment_1\Video_Times\Dataframe_df1a_Vid_1_00' + str(par)) + '.csv', sep=',',) # pilot
 #df1 = np.insert(df2,0,[0],axis = 0) #shift data one row down from the top so we don't miss the first event on o
 #df1 = pd.DataFrame(df1) 
 #df1.columns = ['Frame', 'Event'] # name columns - may need to add ['Adj_Index']
@@ -35,24 +35,22 @@ df1 = pd.read_csv((r'M:\Data\GoPro_Visor\Experiment_1\Video_Times\Dataframe_df1a
 #pi_times = pd.read_csv((r'M:\Data\GoPro_Visor\Experiment_1\Pi_Times\\00' + str(par) + '_visual_p3_gopro_visor.csv'), sep=',') # pilot
 pi_times = np.genfromtxt((r'M:\Data\GoPro_Visor\Experiment_1\Pi_Times\\00' + str(par) + '_visual_p3_gopro_visor.csv'), delimiter=',')
 pi_times = pi_times[0,:]
-pi_times = np.delete(pi_times,(0,4))
+pi_times = np.delete(pi_times,(0))
 pi_times = pi_times.astype(int)
 
-#temp_diff = df1.diff() # take the difference vertically to find the time gap between each event
-#df1 = df1.drop(temp_diff[10 > temp_diff.Time < 0.5].index) # This will get rid of double detections (events within 0.2 seconds of each other)
-
 # Add into the camera time dataframe
-df1['Event_pi'] = pi_times[0:len(df1.index)]
-df1 = df1.drop(df1[(df1.Event_pi == 3)|(df1.Event_pi == np.nan)|(df1.Event_pi == 4)].index) # With the first event aligned - drop any event 3's according to the pi_times
-df1 = df1.drop([0]) # With the first event aligned - drop any event 3's according to the pi_times
-df1 = df1.reset_index() # resets index after removing events
-df1 = df1.drop(columns='index')
+dfpi = pd.DataFrame(pi_times)   # change to a pandas DataFrame
+dfpi.columns = ['Event_pi'] # name columns
+dfpi = dfpi.drop(dfpi[(dfpi.Event_pi != 1) & (dfpi.Event_pi != 2)].index) # With the first event aligned - drop any event 3's according to the pi_times
+dfpi = dfpi.reset_index() # resets index after removing events
+dfpi = dfpi.drop(columns='index')
+df1 = df1.join(dfpi)
 
 # Take out any remain outliers
-temp_diff = df1.diff() # take the difference vertically to find the time gap between each event
-df1 = df1.drop(temp_diff[(temp_diff.Time < 1.5)].index) # |((temp_diff.Time[2:-1] > 3)&(temp_diff.Time[2:1] < 7)) # This will get rid of double detections (events within 0.2 seconds of each other)
-df1 = df1.reset_index() #moves the index over - #df1 = df1.reset_index() # may need a second one to recalibrate index to index_0
-df1 = df1.drop(columns='index')
+#temp_diff = df1.diff() # take the difference vertically to find the time gap between each event
+#df1 = df1.drop(temp_diff[(temp_diff.Time < 1.5)].index) # |((temp_diff.Time[2:-1] > 3)&(temp_diff.Time[2:1] < 7)) # This will get rid of double detections (events within 0.2 seconds of each other)
+#df1 = df1.reset_index() #moves the index over - #df1 = df1.reset_index() # may need a second one to recalibrate index to index_0
+#df1 = df1.drop(columns='index')
 # %% Load in EEG times
 
 filename = 'M:\Data\GoPro_Visor\Experiment_1\EEG_Data\\00' + str(par) + '_GoPro_Visor_Eye_Pi.vhdr' # pilot
@@ -83,8 +81,8 @@ df2 = df2.drop(columns='index')
 df3 = df1.join(df2) # join eeg_times to pi_times
 df3 = df3.reset_index()
 df3['Difference'] = df3['eeg_times'] - df3['Time']
-df3 = df3.drop(columns='index')
-df3 = df3.dropna()
+#df3 = df3.drop(columns='index')
+#df3 = df3.dropna()
 
 ##if diff_outlier == 1 # # Need to take out an outlier? Remember to reset indices
 #temp_df_diff = df3.diff()
@@ -92,21 +90,59 @@ df3 = df3.dropna()
 #df3 = df3.reset_index()
 #df3 = df3.drop(columns='index')
 ##df3 = df3.drop(columns='level_0')
+
+# Offset from one at the start? - adjust one of the coloumns
 #df3.eeg_times = df3.eeg_times.shift(-1) # if the start is offset - push it up and drop the misalinged
-#
-## Manual Adjust
+#df3.Time = df3.Time.shift(-1)
+# %% Manual Adjust
+# For participant 001
+df3['Time'][250:-2] = df3['Time'][252::]
+df3['Event'][250:-2] = df3['Event'][252::]
+df3 = df3.drop([250,251]) 
+df3 = df3.reset_index()
+df3 = df3.drop(columns='index')
+df3['Difference'] = df3['eeg_times'] - df3['Time']
+df3 = df3.drop([429,430]) # take off the same number that were spilt 
+
+#For participant 003
+#df3['Time'][251::] = df3['Time'][250:-1]
+#df3 = df3.drop([249,250]) 
+#df3 = df3.reset_index()
+#df3['Difference'] = df3['eeg_times'] - df3['Time']
+#df3 = df3.drop([437,438])
+
+# For participant 004
+df3['Time'][251:-1] = df3['Time'][252::]
+df3['Event'][251:-1] = df3['Event'][252::]
+df3 = df3.drop([250]) 
+df3 = df3.reset_index()
+df3 = df3.drop(columns='index')
+df3['Difference'] = df3['eeg_times'] - df3['Time']
+df3 = df3.drop([440])
+
+# For participant 005
+df3['Time'][250:-1] = df3['Time'][251::] 
+df3 = df3.reset_index()
+df3['Difference'] = df3['eeg_times'] - df3['Time']
+df3 = df3.drop([415])
+
+
+
+df3['Event_Difference'] = df3['Event'] - df3['Event_Type']
+
+# save after processing
+export_csv = df3.to_csv (r'M:\Data\GoPro_Visor\Experiment_1\Video_Times\Dataframe_df3_final_00' + str(par) + '.csv', index = None, header=True) #Don't forget to add '.csv' at the end of the path
+# or load in
+df3 = pd.read_csv((r'M:\Data\GoPro_Visor\Experiment_1\Video_Times\Dataframe_df3_final_00' + str(par)) + '.csv', sep=',') # pilot
+
+
 #df3['eeg_times'][247:-2] = df3['eeg_times'][249::]
-##    df3 = np.append(df3, np.zeros((3,6)), axis=0)
+#df3 = np.append(df3, np.zeros((3,6)), axis=0)
 #df3 = np.insert(df2,0,[0],axis = 0) #shift data one row down from the top so we don't miss the first event on o
 #df3 = df3.dropna()
-#df3 = df3.reset_index()
-#    
-
-
 # any remaining offset are missed frames
 # find the remaining offsets and readjust to the mean
 #df3['Difference'][248:-1].index = df3['Difference'][248:-1].index+1
-
 #Targ_Std_fin = df1.drop(Targ_Std_diff[Targ_Std_diff.Time < 0.2].index)
 
 # Latency plot
@@ -123,9 +159,9 @@ plt.show()
 
 # Difference plot
 plt.figure(1)
-plt.plot(df3['Difference'], df3['index'], label='EEG - Pi')
+plt.plot(df3['Difference']*1000, df3['index'], label='EEG - Pi')
 legend = plt.legend(loc='upper left', shadow=True, fontsize='x-large')
-plt.xlabel('Latency (Seconds)')
+plt.xlabel('Latency (ms)')
 plt.ylabel('Trial Number')
 plt.title('Trial Number vs Difference - Par_00{}'.format(par))
 plt.show()
@@ -133,6 +169,7 @@ plt.show()
 # Histogram Plots
 # Event Latency
 # EEG
+plt.figure(2)
 df3['EEG Times (s)'] = df3['eeg_times']
 plt.figure(figsize=(15,10))
 plt.tight_layout()
@@ -140,6 +177,7 @@ plt.title('EEG Event Latency Distribution - Par_00{}'.format(par))
 plt.ylabel('Number of Trials')
 sns.distplot(df3['EEG Times (s)'])
 # Camera
+plt.figure(3)
 df3['Camera Times (s)'] = df3['Time']
 plt.figure(figsize=(15,10))
 plt.tight_layout()
@@ -147,12 +185,23 @@ plt.title('Camera Event Latency Distribution - Par_00{}'.format(par))
 plt.ylabel('Number of Trials')
 sns.distplot(df3['Camera Times (s)'])
 # Difference
+df3['Difference Times (s)'] = df3['Time']
+plt.figure(4)
 plt.figure(figsize=(15,10))
 plt.tight_layout()
 plt.title('EEG-Camera Difference Distribution - Par_00{}'.format(par))
 plt.ylabel('Number of Trials')
-sns.distplot(df3['Difference'])
+sns.distplot(df3['Difference Times (s)'])
 
+plt.figure(5)
+plt.plot(df3['Event_Difference'], df3['index'], label='EEG - Pi Event')
+legend = plt.legend(loc='upper left', shadow=True, fontsize='x-large')
+plt.xlabel('Latency (Seconds)')
+plt.ylabel('Trial Number')
+plt.title('Trial Number vs Event Difference - Par_00{}'.format(par))
+plt.xlim(-1,1,1)
+plt.xticks((range(-2,3,1)),(range(-2,3,1)))
+plt.show()
 
 
 
@@ -160,11 +209,16 @@ sns.distplot(df3['Difference'])
 
 # %% ## All Point Transform 
 
+# Par specific graph aspects
+#x_ticks = [(0,0,0),(0, 100, 5)]
+
+
 # Prep Data
-trials = 434
+trial_nums = [0,429,0,344,440,415,437]
+trials = trial_nums[par]
 df4 = df3.copy() # copy DataFrame 
 df4 = df4.values # convert from Pandas DataFrame to a numpy structure
-df4 = np.append(df4, np.zeros((trials,3)), axis=1)
+df4 = np.append(df4, np.zeros((trials,6)), axis=1)
 X1 = df4[:,1].reshape(-1,1)
 y1 = df4[:,4].reshape(-1,1)
 
@@ -172,31 +226,36 @@ y1 = df4[:,4].reshape(-1,1)
 model1 = LinearRegression()
 reg =  model1.fit(X1,y1) # From the pi times we are predicting the eeg times
 reg.score(df4[:,1].reshape(-1,1), df4[:,4].reshape(-1,1))
-df4[:,6] = df4[:,1]*reg.coef_ + reg.intercept_  # eeg times = camera_times X slope of 
-df4[:,7] = df4[:,4]-df4[:,6]
-# 1:index , 2:pi times, 3:pi events, 1:eeg_times, 2:eeg_trig 5:difference, 6:transformed difference, 7:difference between original difference and transformed difference
+df4[:,10] = df4[:,1]*reg.coef_ + reg.intercept_  # eeg times = camera_times X slope of 
+df4[:,11] = df4[:,4]-df4[:,10]
+# 0:index , 1:pi times, 2:camera events, 3:pi_events, 4:eeg_times, 5:eeg_trig, 6:difference, 10:transformed difference, 11:difference between original difference and transformed difference
 
+df3['AP_Transform'] = df4[:,11]
 # All Point Transform 
 # Difference plots
 # Line Plot
-plt.figure(123)
-plt.plot(df4[:,7], df4[:,0])
+plt.figure(6)
+plt.plot(df4[:,11]*1000, df4[:,0])
 #plt.plot(df4[:,10], df4[:,0]) #plot the magnitude of the difference 
 #plt.plot(df3['Difference'], df3['level_0'], label='EEG - Pi') # plot untransformed
 plt.legend('EEG - Pi', ncol=2, loc='upper left'); #  scalex=True, scaley=True if not using a custom xticks arguement
-plt.xlabel('Latency (miliseconds)')
+plt.xlabel('Latency (ms)')
 plt.ylabel('Trial Number')
 plt.title('Trial Number vs Transformed Difference - Par_00{}'.format(par))
+#plt.xticks(np.arange(-0.002,0.003,0.001),(range(-2,3,1)))
 #plt.xlim([-0.00001, 0.00001])
 plt.show()
 
+
+
 # Distribution
+plt.figure(6)
 plt.figure(figsize=(15,10))
 plt.tight_layout()
-plt.title('EEG-Camera Difference Distribution - Par_00{}'.format(par))
+plt.title('All Point Transformed EEG-Camera Difference Distribution - Par_00{}'.format(par))
 plt.ylabel('Number of Trials')
-plt.xlabel('Difference (seconds)')
-sns.distplot(df4[:,7], rug = True, rug_kws={'color': 'black'})
+plt.xlabel('Difference (ms)')
+sns.distplot(df4[:,11]*1000, rug = True, rug_kws={'color': 'black'})
 
 
 
@@ -213,13 +272,13 @@ y2 = df5[:,1].reshape(-1,1) # eeg first & last
 
 #Equate and Test Regression
 model1 = LinearRegression()
-reg =  model1.fit(X,y) # From the pi times we are predicting the eeg times
+reg =  model1.fit(X2,y2) # From the pi times we are predicting the eeg times
 reg.score(df5[:,1].reshape(-1,1), df5[:,3].reshape(-1,1))
 df5[:,6] = df5[:,1]*reg.coef_ + reg.intercept_  # eeg times = camera_times X slope of 
 df5[:,7] = df5[:,3]-df5[:,6]
 # 1:index , 2:pi times, 3:pi events, 1:eeg_times, 2:eeg_trig 5:difference, 6:transformed difference, 7:difference between original difference and transformed difference
 
-
+df3['Two_Point_Transform'] = df4[:,11]
 # All Point Transform 
 # Difference plots
 # Line Plot
@@ -253,44 +312,30 @@ df6 = df3.copy() # copy DataFrame
 df6 = df6.values # convert from Pandas DataFrame to a numpy structure
 df6 = np.append(df6, np.zeros((trials,3)), axis=1) 
 
-1X3 = df6[0:10,3:5] # Pull out first 10 event from camera
-2X3 = df6[190:-1,3:5] # Pull out first 10 event from camera
+X3 = np.zeros([20,1])
+X3[0:10] = df6[0:10,1:2] # Pull out first 10 event from camera
+X3[10::] = df6[-11:-1,1:2] # Pull out first 10 event from camera
 
-#1X3[:,1] = np.around(X3[:,1]), 
-#1X3.astype(float) 
-#2X3[:,1] = np.around(y3[:,1]), 
-#2X3.astype(float)
-#1X3 = 1X3.reshape(-1,1)
-#2X3 = 2X3.reshape(-1,1)
-X3 = np.concatenate(1X3, 2X3) # Combine first and last 10
-
-1y3 = df6[0:10,3:5] # Pull out first 10 event from camera
-2y3 = df6[190:-1,3:5] # Pull out first 10 event from camera
-
-#1y3[:,1] = np.around(1y3[:,1]), 
-#1y3.astype(float) 
-#2y3[:,1] = np.around(2y3[:,1]), 
-#2y3.astype(float)
-#1y3 = 1y3.reshape(-1,1)
-#2y3 = 2y3.reshape(-1,1)
-y3 = np.concatenate(1y3, 2y3) # Combine first and last 10
-
+y3 = np.zeros([20,1])
+y3[0:10] = df6[0:10,4:5] # Pull out first 10 event from camera
+y3[10::] = df6[-11:-1,4:5] # Pull out first 10 event from camera
 
 #Equate and Test Regression
 model1 = LinearRegression()
 reg =  model1.fit(X3,y3) # From the pi times we are predicting the eeg times
-reg.score(df6[:,1].reshape(-1,1), df6[:,3].reshape(-1,1))
+reg.score(df6[:,1].reshape(-1,1), df6[:,4].reshape(-1,1))
 df6[:,6] = df6[:,1]*reg.coef_ + reg.intercept_  # eeg times = camera_times X slope of 
 df6[:,7] = df6[:,3]-df6[:,6]
 # 1:index , 2:pi times, 3:pi events, 1:eeg_times, 2:eeg_trig 5:difference, 6:transformed difference, 7:difference between original difference and transformed difference
 
+df3['Tail_Transform_10_Point'] = df6[:,8]
 
 # All Point Transform 
 # Difference plots
 # Line Plot
 plt.figure(123)
-plt.plot(df4[:,7], df4[:,0])
-#plt.plot(df4[:,10], df4[:,0]) #plot the magnitude of the difference 
+plt.plot(df3['Tail_Transform_10_Point'], df3['index'])
+#plt.plot(df6[:,10], df4[:,0]) #plot the magnitude of the difference 
 #plt.plot(df3['Difference'], df3['level_0'], label='EEG - Pi') # plot untransformed
 plt.legend('EEG - Pi', ncol=2, loc='upper left'); #  scalex=True, scaley=True if not using a custom xticks arguement
 plt.xlabel('Latency (miliseconds)')
@@ -305,11 +350,11 @@ plt.tight_layout()
 plt.title('EEG-Camera Difference Distribution - Par_00{}'.format(par))
 plt.ylabel('Number of Trials')
 plt.xlabel('Difference (seconds)')
-sns.distplot(df4[:,7], rug = True, rug_kws={'color': 'black'})
+sns.distplot(df3['Tail_Transform_10_Point'], rug = True, rug_kws={'color': 'black'})
 
 #%% Comparison of Difference between EEG and Camera Times after each Type of Transforms and Raw
 Raw_Diff_Sum = sum(abs(df3['Difference']))
-All_Point_Sum = 
-Two_Point_Sum = 
-Ten_Tail_Sum = 
+All_Point_Sum = sum(abs(df3['AP_Transform']))
+Ten_Tail_Sum = sum(abs(df3['Tail_Transform_10_Point']))
+
 
---
+++ Video Analysis/Group_ Figures.py
@@ -0,0 +1,277 @@
+import numpy as np
+import mne
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from matplotlib import rc
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+#rc('text', usetex=True)
+
+
+# Initialize variables and plot
+par_num = 4
+# Load in participant dataframes {001,003,004,005}
+for par in range(1,par_num+2):
+    if par != 2:
+        if par > 2:
+            globals()["df_" + str(par-1)] = pd.read_csv((r'M:\Data\GoPro_Visor\Experiment_1\Video_Times\Dataframe_df3_final_00' + str(par)) + '.csv', sep=',') # pilot
+
+        else:
+            globals()["df_" + str(par)] = pd.read_csv((r'M:\Data\GoPro_Visor\Experiment_1\Video_Times\Dataframe_df3_final_00' + str(par)) + '.csv', sep=',') # pilot
+
+#par = 4
+#        # Add each dataframe to plot
+#for ax, par, name in zip(axes.flatten(), globals()["df_" + str(par)], par):
+#    if par != 2:
+##        ax = fig.add_subplot(2, 3, par)
+#        sns.distplot((globals()["df_" + str(par)][11,:],globals()["df_" + str(par)]['index']), hist=False) #label='EEG - Pi Event'
+#        ax.set(title = 1, xlabel='ms')
+#plt.show()    
+
+
+# %% Raw Difference -  trial count
+fig, ax = plt.subplots(figsize=(10,5), ncols=2, nrows=2)
+
+left   =  0.125  # the left side of the subplots of the figure
+right  =  0.9    # the right side of the subplots of the figure
+bottom =  0.1    # the bottom of the subplots of the figure
+top    =  0.9    # the top of the subplots of the figure
+wspace =  .5     # the amount of width reserved for blank space between subplots
+hspace =  1.1    # the amount of height reserved for white space between subplots
+
+# This function actually adjusts the sub plots using the above paramters
+plt.subplots_adjust(
+    left    =  left, 
+    bottom  =  bottom, 
+    right   =  right, 
+    top     =  top, 
+    wspace  =  wspace, 
+    hspace  =  hspace
+)
+# The amount of space above titles
+y_title_margin = 1 
+fig.suptitle("Raw Difference - Trial Count vs Difference", fontsize=20)
+
+# In statistics, kernel density estimation (KDE) is a non-parametric way to estimate the probability density function (PDF) of a random variable.  # norm_hist=False, kde=False
+#In the following plots density refers to kernel density estimation (KDE) a non-parametric estimation of the probability density function
+### 001,003
+ax[0][0].set_title("001",                    y = y_title_margin)
+sns.distplot(df_1['Difference']*1000, bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[0][0])
+ax[0][1].set_title("003",         y = y_title_margin)
+sns.distplot(df_2['Difference']*1000, bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[0][1])
+
+[ax[0][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[0][i].set_ylabel("Trial Count") for i in range(0, 2)]
+
+
+# 004,005
+ax[1][0].set_title("004",                  y = y_title_margin)
+sns.distplot(df_3['Difference']*1000, bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[1][0])
+ax[1][1].set_title("005",       y = y_title_margin)
+sns.distplot(df_4['Difference']*1000, bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[1][1])
+#ax[1][1].set_xlim([-60,5])
+
+[ax[1][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[1][i].set_ylabel("Trial Count") for i in range(0, 2)]
+
+# %% Raw Difference -  trial density
+fig, ax = plt.subplots(figsize=(10,5), ncols=2, nrows=2)
+
+left   =  0.125  # the left side of the subplots of the figure
+right  =  0.9    # the right side of the subplots of the figure
+bottom =  0.1    # the bottom of the subplots of the figure
+top    =  0.9    # the top of the subplots of the figure
+wspace =  .5     # the amount of width reserved for blank space between subplots
+hspace =  1.1    # the amount of height reserved for white space between subplots
+
+# This function actually adjusts the sub plots using the above paramters
+plt.subplots_adjust(
+    left    =  left, 
+    bottom  =  bottom, 
+    right   =  right, 
+    top     =  top, 
+    wspace  =  wspace, 
+    hspace  =  hspace
+)
+# The amount of space above titles
+y_title_margin = 1 
+fig.suptitle("Raw Difference - Trial Density vs Difference", fontsize=20)
+
+# In statistics, kernel density estimation (KDE) is a non-parametric way to estimate the probability density function (PDF) of a random variable.  # norm_hist=False, kde=False
+
+### 001,003
+ax[0][0].set_title("001",                    y = y_title_margin)
+sns.distplot(df_1['Difference']*1000, bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[0][0])
+ax[0][1].set_title("003",         y = y_title_margin)
+sns.distplot(df_2['Difference']*1000, bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[0][1])
+
+[ax[0][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[0][i].set_ylabel("Trial Ratio") for i in range(0, 2)]
+
+
+# 004,005
+ax[1][0].set_title("004",                  y = y_title_margin)
+sns.distplot(df_3['Difference']*1000, bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[1][0])
+ax[1][1].set_title("005",       y = y_title_margin)
+sns.distplot(df_4['Difference']*1000, bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[1][1])
+#ax[1][1].set_xlim([-60,5])
+
+[ax[1][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[1][i].set_ylabel("Trial Ratio") for i in range(0, 2)]
+
+
+# %% AP Transformed Difference - trial count
+
+fig, ax = plt.subplots(figsize=(10,5), ncols=2, nrows=2)
+#plt.rc('text', usetex=True)
+#plt.rc('font', family='serif')
+
+
+left   =  0.125  # the left side of the subplots of the figure
+right  =  0.9    # the right side of the subplots of the figure
+bottom =  0.1    # the bottom of the subplots of the figure
+top    =  0.9    # the top of the subplots of the figure
+wspace =  .5     # the amount of width reserved for blank space between subplots
+hspace =  1.1    # the amount of height reserved for white space between subplots
+
+# This function actually adjusts the sub plots using the above paramters
+plt.subplots_adjust(
+    left    =  left, 
+    bottom  =  bottom, 
+    right   =  right, 
+    top     =  top, 
+    wspace  =  wspace, 
+    hspace  =  hspace
+)
+
+# The amount of space above titles
+y_title_margin = 1
+fig.suptitle("All Point Transformed Difference - Trial Count vs. Difference", fontsize=20)
+
+### Bathrooms
+ax[0][0].set_title("001",                    y = y_title_margin)
+sns.distplot(df_1['AP_Transform']*1000,  bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[0][0])
+
+ax[0][1].set_title("003",         y = y_title_margin)
+sns.distplot(df_2['AP_Transform']*1000,  bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[0][1])
+
+[ax[0][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[0][i].set_ylabel("Trial Ratio") for i in range(0, 2)]
+
+### Square feet
+ax[1][0].set_title("004",                  y = y_title_margin)
+sns.distplot(df_3['AP_Transform']*1000, bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[1][0])
+
+ax[1][1].set_title("005",       y = y_title_margin)
+sns.distplot(df_4['AP_Transform']*1000, bins = 12, kde=False, rug = True, rug_kws={'color': 'black'}, ax=ax[1][1])
+ax[1][1].set_xlim([-6,6])
+#ax[1][1].text(0.5, 0.5, '\TeX\ hello world: $\int_0^\infty e^x dx$', size=20, ha='center', va='center')
+
+
+[ax[1][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[1][i].set_ylabel("Trial Ratio") for i in range(0, 2)]
+
+
+# %% AP Transformed Difference - trial density
+fig, ax = plt.subplots(figsize=(10,5), ncols=2, nrows=2)
+#plt.rc('text', usetex=True)
+#plt.rc('font', family='serif')
+
+
+left   =  0.125  # the left side of the subplots of the figure
+right  =  0.9    # the right side of the subplots of the figure
+bottom =  0.1    # the bottom of the subplots of the figure
+top    =  0.9    # the top of the subplots of the figure
+wspace =  .5     # the amount of width reserved for blank space between subplots
+hspace =  1.1    # the amount of height reserved for white space between subplots
+
+# This function actually adjusts the sub plots using the above paramters
+plt.subplots_adjust(
+    left    =  left, 
+    bottom  =  bottom, 
+    right   =  right, 
+    top     =  top, 
+    wspace  =  wspace, 
+    hspace  =  hspace
+)
+
+# The amount of space above titles
+y_title_margin = 1
+fig.suptitle("All Point Transformed Difference - Trial Density vs Difference", fontsize=20)
+
+### Bathrooms
+ax[0][0].set_title("001",                    y = y_title_margin)
+sns.distplot(df_1['AP_Transform']*1000,  bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[0][0])
+
+ax[0][1].set_title("003",         y = y_title_margin)
+sns.distplot(df_2['AP_Transform']*1000,  bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[0][1])
+
+[ax[0][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[0][i].set_ylabel("Trial Ratio") for i in range(0, 2)]
+
+### Square feet
+ax[1][0].set_title("004",                  y = y_title_margin)
+sns.distplot(df_3['AP_Transform']*1000, bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[1][0])
+
+ax[1][1].set_title("005",       y = y_title_margin)
+sns.distplot(df_4['AP_Transform']*1000, bins = 12, rug = True, rug_kws={'color': 'black'}, ax=ax[1][1])
+ax[1][1].set_xlim([-6,6])
+#ax[1][1].text(0.5, 0.5, '\TeX\ hello world: $\int_0^\infty e^x dx$', size=20, ha='center', va='center')
+
+
+[ax[1][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[1][i].set_ylabel("Trial Ratio") for i in range(0, 2)]
+
+
+
+#%% Tail Transform
+fig, ax = plt.subplots(figsize=(10,5), ncols=2, nrows=2)
+#plt.rc('text', usetex=True)
+#plt.rc('font', family='serif')
+
+
+left   =  0.125  # the left side of the subplots of the figure
+right  =  0.9    # the right side of the subplots of the figure
+bottom =  0.1    # the bottom of the subplots of the figure
+top    =  0.9    # the top of the subplots of the figure
+wspace =  .5     # the amount of width reserved for blank space between subplots
+hspace =  1.1    # the amount of height reserved for white space between subplots
+
+# This function actually adjusts the sub plots using the above paramters
+plt.subplots_adjust(
+    left    =  left, 
+    bottom  =  bottom, 
+    right   =  right, 
+    top     =  top, 
+    wspace  =  wspace, 
+    hspace  =  hspace
+)
+
+# The amount of space above titles
+y_title_margin = 1
+fig.suptitle("All Point Transformed Difference", fontsize=20)
+
+### Bathrooms
+ax[0][0].set_title("001",                    y = y_title_margin)
+sns.distplot(df_1['AP_Transform']*1000,       rug = True, rug_kws={'color': 'black'}, ax=ax[0][0])
+
+ax[0][1].set_title("003",         y = y_title_margin)
+sns.distplot(df_2['AP_Transform']*1000,   rug = True, rug_kws={'color': 'black'}, ax=ax[0][1])
+
+[ax[0][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[0][i].set_ylabel("Trial Count") for i in range(0, 2)]
+
+### Square feet
+ax[1][0].set_title("004",                  y = y_title_margin)
+sns.distplot(df_3['AP_Transform']*1000, rug = True, rug_kws={'color': 'black'}, ax=ax[1][0])
+
+ax[1][1].set_title("005",       y = y_title_margin)
+sns.distplot(df_4['AP_Transform']*1000, bins=8, rug = True, rug_kws={'color': 'black'}, ax=ax[1][1])
+ax[1][1].set_xlim([-6,6])
+#ax[1][1].text(0.5, 0.5, '\TeX\ hello world: $\int_0^\infty e^x dx$', size=20, ha='center', va='center')
+
+
+[ax[1][i].set_xlabel("ms") for i in range(0, 2)]
+[ax[1][i].set_ylabel("Trial Count") for i in range(0, 2)]
